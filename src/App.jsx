3035f367da36bc4e6f71a519a8e463709fc9dd7d
DESKTOP-50BHF1U\user <cedomir@antecna.com>
2021-03-23 14:14:48 +0100

    provjera dodavanja svega promjenjenog

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-import styled from 'styled-components';
-import Host from './components/host/Host';
-import { Flex } from 'reflexbox/styled-components';
 import Toolbar from './components/navigation/toolbar/Toolbar';
 import { BrowserRouter } from "react-router-dom";
 import MRouter from './router/MRouter';
-import Home from './pages/home/Home';
 
 const App = () => {
 
