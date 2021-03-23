5b6e5a636b445b7d246933ceaee41f6fefa01ad7
DESKTOP-50BHF1U\user <cedomir@antecna.com>
2021-03-23 16:02:01 +0100

    Routeru dodana putanja

--- src/router/MRouter.jsx
+++ src/router/MRouter.jsx
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router';
 import Home from '../pages/home/Home';
 import Services from '../pages/services/Services';
 import Hosts from '../pages/hosts/Hosts';
+import Service from '../pages/service/Service';
 
 const MRouter = (props) => {
 
     return (
             <Switch>
                 <Route path="/hosts" component={Hosts} />
+                <Route path="/service" component={Service} />
                 <Route path="/services" component={Services} />
                 <Route path="/" component={Home} />
             </Switch>
