634d448a49ea1220d1e338b16b8e913ef6849d74
DESKTOP-50BHF1U\user <cedomir@antecna.com>
2021-03-23 15:44:14 +0100

    dodan login

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import Dashboard from '../../components/dashboard/Dashboard';
+
+const Form = styled(Flex)`
+    flex-direction: column;
+    box-shadow: 0px 5px 37px -12px rgba(0,0,0,0.75);
+    height: 50%;
+    width: 40%;
+    align-items: center;
+`;
+
+const Login = (props) => {
+
+    return(
+        <Dashboard>
+            <Form>
+                asdas
+            </Form>
+        </Dashboard>
+    );
+
+};
+
+export default Login;
---
+++ src/pages/service/Service.jsx
@@ -0,0 +1,9 @@
+import Dashboard from '../../components/dashboard/Dashboard';
+
+const Service = () => {
+    return(
+        <Dashboard>Servis</Dashboard>
+    );
+};
+
+export default Service;
