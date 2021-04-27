2a5414f89d3479fd82f7eeb29c420fa34f35c4fe
DESKTOP-50BHF1U\user <cedomir@antecna.com>
2021-04-27 09:25:44 +0200

    config file added

---
+++ src/common/config/config.js
@@ -0,0 +1,4 @@
+export const nagiosAuthFailedMessage = "Authenticiation failed.";
+export const authFailed = "Auth failed";
+export const basicColor = "gainsboro";
+export const spinnerColor = basicColor;
